95c32d4bd42e0d12721c795bfef9077598747ea7
Devante <GitEntity@users.noreply.github.com>
2018-02-21 16:29:52 -0500

    Fixed classification and .csv write
    Fixed classification where train_test_split was used when it did not need to be. Writing to the .csv file also fixed.

--- NBAShotPrediction/nbashotclassifier.py
+++ NBAShotPrediction/nbashotclassifier.py
@@ -26,14 +26,14 @@ def test_data_loaded(data_frame):
         and thus, cannot be indexed. When dropping old features, this header must be removed from the list.
         The 'EVENT_TYPE' header is the same thing, so it is used instead."""
     # drop header from the data frame
-    data_frame = nba.drop(['ACTION_TYPE', 'EVENTTIME', 'EVENT_TYPE', 'GAME_DATE', 'GAME_EVENT_ID', 'GAME_ID', 'HTM',
+    data_frame = nba.drop(['ACTION_TYPE', 'EVENTTIME', 'EVENT_TYPE', 'GAME_DATE', 'GAME_ID', 'HTM',
                 'MINUTES_REMAINING', 'PERIOD', 'PLAYER_ID', 'PLAYER_NAME', 'QUARTER', 'SECONDS_REMAINING',
                 'SHOT_ATTEMPTED_FLAG', 'SHOT_TIME', 'SHOT_TYPE', 'SHOT_ZONE_AREA', 'SHOT_ZONE_BASIC',
                 'SHOT_ZONE_RANGE', 'TEAM_ID', 'TEAM_NAME', 'VTM', 'LOC_X', 'LOC_Y'], axis=1)
     return data_frame
 
 # define file name
-file_name = 'train.csv'
+file_name = 'solution_no_answer.csv'
 # load the NBA shot data set using Pandas
 nba = pd.read_csv(file_name)
 
@@ -58,7 +58,7 @@ if file_name=='solution_no_answer.csv':
     nba = test_data_loaded(nba)
 else: # training or validation set are being used
     # remove old features
-    nba = nba.drop(['ACTION_TYPE', 'EVENTTIME', 'EVENT_TYPE', 'GAME_DATE', 'GAME_EVENT_ID', 'GAME_ID', 'HTM',
+    nba = nba.drop(['ACTION_TYPE', 'EVENTTIME', 'EVENT_TYPE', 'GAME_DATE', 'GAME_ID', 'HTM',
                     'MINUTES_REMAINING', 'PERIOD', 'PLAYER_ID', 'PLAYER_NAME', 'QUARTER', 'SECONDS_REMAINING',
                     'SHOT_ATTEMPTED_FLAG', 'SHOT_MADE_FLAG', 'SHOT_TIME', 'SHOT_TYPE', 'SHOT_ZONE_AREA', 'SHOT_ZONE_BASIC',
                     'SHOT_ZONE_RANGE', 'TEAM_ID', 'TEAM_NAME', 'VTM', 'LOC_X', 'LOC_Y'], axis=1)
@@ -69,24 +69,28 @@ nba = pd.concat([nba, action_type, event_type, shot_type, shot_zone_area, shot_z
 # define prediction target
 y = nba['Missed Shot']
 # set prediction data (return view/copy with column(s) removed)
-X = nba.drop('Missed Shot', axis=1)
-
-# create a randomized train and test split with scikit-learn
-# and withhold all test data from model when training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+X = nba.drop(['Missed Shot', 'GAME_EVENT_ID'], axis=1)
+# create view that still includes 'GAME_EVENT_ID' header to merge into submission
+nba_2 = nba.drop('Missed Shot', axis=1)
 
 # create logistic regression model (transformed = 1 / (1 + e^-x))
 logmodel = LogisticRegression()
 
 # fit/train the model on the training data
-logmodel.fit(X_train, y_train)
+logmodel.fit(X, y)
 
 # generate predictions for the withheld test data
-predictions = logmodel.predict(X_test)
+predictions = logmodel.predict(X)
+
+# create submission for Kaggle
+submission = pd.DataFrame({"GAME_EVENT_ID": nba_2["GAME_EVENT_ID"], "SHOT_MADE_FLAG": predictions})
+
+# write submission to csv
+submission.to_csv("logreg_submission.csv", index=False) # do not save index values
 
 # evaluate performance of the model
-print(classification_report(y_test, predictions))
-print("Accuracy: {}".format(accuracy_score(y_test, predictions)))
+print(classification_report(y, predictions))
+print("Accuracy: {}".format(accuracy_score(y, predictions))) # can alternatively use model.score(X, y)
 
 # # standard scale all the training data
 # scaler = StandardScaler()
